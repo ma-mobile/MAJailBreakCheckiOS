8b46e8fef28795c0279b7162d5e7de339aa073fb
Harikrishnan V S <117502319+vshari96@users.noreply.github.com>
2025-10-10 15:13:54 +0530

    minor fixes

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     name: "MAJailBreakCheck", // Replace with your desired package name
     platforms: [
         .iOS(.v13), // Specify platforms and versions your package supports
-        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -27,6 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MAJailBreakCheck",
-            dependencies: [])
+            dependencies: [],
+            path: "Sources/MAJailBreakCheck")
     ]
 )
