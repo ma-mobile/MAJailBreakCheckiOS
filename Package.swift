bd01e6fe740d38e20a73806050459d3f49133010
Harikrishnan V S <117502319+vshari96@users.noreply.github.com>
2025-10-10 15:01:41 +0530

    refactoring for SPM package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+//
+//  Package.swift
+//  MAJailBreakCheck
+//
+//  Created by Harikrishnan V S (MA-IN) on 10/10/25.
+//
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MAJailBreakCheck", // Replace with your desired package name
+    platforms: [
+        .iOS(.v13), // Specify platforms and versions your package supports
+        .macOS(.v10_15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MAJailBreakCheck",
+            targets: ["MAJailBreakCheck"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MAJailBreakCheck",
+            dependencies: [])
+    ]
+)
