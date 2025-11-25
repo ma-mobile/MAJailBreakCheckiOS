c247885ca615095566f063827627aaa319484ba5
ma-mobile <intdcmobile@gmail.com>
2025-11-25 16:18:38 +0530

    Remove stack protection flags from MAJailBreakCheck
    Removed unsafe flags for stack protection from target settings.

--- Package.swift
+++ Package.swift
@@ -25,16 +25,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MAJailBreakCheck",
-            cSettings: [
-                .unsafeFlags(["-fstack-protector-strong"])
-            ],
-            cxxSettings: [
-                .unsafeFlags(["-fstack-protector-strong"]) // Enable stack protection for C code
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-fstack-protector-strong"]) // Enable stack protection for C code
-            ],    
+            name: "MAJailBreakCheck",  
             dependencies: [],
             path: "Sources/MAJailBreakCheck")
     ]
